b516a35737bb1aa5f0c8839974aaec5972bf74db
itepechi <72330683+itepechi@users.noreply.github.com>
2022-01-11 04:30:49 +0900

    Revert "fix: add delay option to macro key"
    This reverts commit bd226c4d02d7f360747364a59cc5f0da50524f2c.

--- lua/which-key/plugins/registers.lua
+++ lua/which-key/plugins/registers.lua
@@ -4,7 +4,7 @@ M.name = "registers"
 
 M.actions = {
   { trigger = '"', mode = "n" },
-  { trigger = "@", mode = "n", delay = true },
+  { trigger = "@", mode = "n" },
   { trigger = "<c-r>", mode = "i" },
   { trigger = "<c-r>", mode = "c" },
 }
@@ -15,9 +15,7 @@ function M.setup(_wk, config, options)
   end
 
   for _, action in ipairs(M.actions) do
-    if not action.delay then
-      table.insert(options.triggers_nowait, action.trigger)
-    end
+    table.insert(options.triggers_nowait, action.trigger)
   end
 end
 
